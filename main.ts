ee95f5b2ee99472ef7c42564774aff1574464704
Hiptoniel <130452973+Hiptoniel@users.noreply.github.com>
2023-05-23 12:16:49 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -16,22 +16,16 @@ basic.forever(function () {
     if (pasos == 100) {
         music.playMelody("A B G E C5 A G F ", 120)
     }
-})
-basic.forever(function () {
-    if (input.acceleration(Dimension.X) > 1800) {
-        pasos += 1
+    if (input.acceleration(Dimension.X) > 1500) {
+        pasos += 2
         basic.pause(200)
     }
-})
-basic.forever(function () {
-    if (input.acceleration(Dimension.Y) > 3000) {
-        pasos += 1
+    if (input.acceleration(Dimension.Y) > 1500) {
+        pasos += 2
         basic.pause(200)
     }
-})
-basic.forever(function () {
     if (input.acceleration(Dimension.Z) > 1500) {
-        pasos += 1
+        pasos += 2
         basic.pause(200)
     }
 })
