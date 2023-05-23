440008f9412c372e3181235fdcb0726cee93017c
Hiptoniel <130452973+Hiptoniel@users.noreply.github.com>
2023-05-23 12:05:48 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
 })
 input.onButtonPressed(Button.AB, function () {
-    basic.showNumber(0.713 * pasos)
+    basic.showNumber(0.76 * pasos)
 })
 input.onButtonPressed(Button.B, function () {
     pasos = 0
@@ -13,7 +13,24 @@ let pasos = 0
 pasos = 0
 basic.showNumber(0)
 basic.forever(function () {
-    if (input.acceleration(Dimension.Strength) > 2500) {
+    if (pasos == 100) {
+        music.playMelody("A B G E C5 A G F ", 120)
+    }
+})
+basic.forever(function () {
+    if (input.acceleration(Dimension.X) > 1800) {
+        pasos += 1
+        basic.pause(200)
+    }
+})
+basic.forever(function () {
+    if (input.acceleration(Dimension.Y) > 3000) {
+        pasos += 1
+        basic.pause(200)
+    }
+})
+basic.forever(function () {
+    if (input.acceleration(Dimension.Z) > 1500) {
         pasos += 1
         basic.pause(200)
     }
